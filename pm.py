ab3da2c63d22cbc92eb923f40107d6ffb72114e4
jaepil-choi <chljeffreyz@gmail.com>
2022-01-24 05:42:41 +0900

    Experimenting pm module with ABC and YAML config

--- pm.py
+++ pm.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 from abc import ABC, abstractmethod
 
+import quantstats as qs
+
 ## custom lib
 from config import PathConfig, PMConfig
+
 
 class BasePM(ABC):
     @property
@@ -26,18 +29,38 @@ class BasePM(ABC):
 
     @property
     @abstractmethod
-    def parameters(self):
+    def hyperparams(self):
         raise NotImplementedError
-    
-    
 
-
-class PM(BasePM):
-    name = "price_sma_momentum"
-    parameters = ["sma_days"]
-    load_path = PathConfig.data_path
-
+class DefaultPM(BasePM):
     def __init__(self) -> None:
         super().__init__()
 
+
+    def plot_result(self, returns):
+        qs.reports.full(returns, benchmark="SPY")
+
+
+class PM(DefaultPM):
+    name = "price_sma_momentum"
+    description = "Simple price / SMA crossover momentum for a single asset"
+    hyperparams = ["sma_days"]
+    load_path = PathConfig.data_path
+    scheme = "D-0"
+
+    def __init__(
+        self, 
+        pm_config,
+        ) -> None:
+
+        super().__init__()
+        for k, v in pm_config.items():
+            # if k in classvars
+            setattr(PM, k, v)
+
+            # if k in instancevars
+            setattr(self, k, v)
+            
+        
+        
         
